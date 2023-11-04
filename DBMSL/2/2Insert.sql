f33dc46b5d0afd618d45382b17c63842e8e9e2d9
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-11-04 08:45:19 +0530

    Inserted data into the tables

---
+++ DBMSL/2/2Insert.sql
@@ -0,0 +1,52 @@
+INSERT INTO Dept VALUES
+	(1, "Computer", "Pune"),
+	(2, "IT", "Kolhapur"),
+	(3, "ENTC", "Mumbai")
+;
+
+INSERT INTO Employee VALUES 
+	(53,2,'voluptas','adipisci','Virtual Assistant',436894,'2004-12-03'),
+	(130,2,'distinctio','labore','Customer Service',233644,'2016-06-21'),
+	(77,1,'vitae','quasi','Customer Support',493742,'1991-10-20'),
+	(81,3,'soluta','non','Help Desk',695828,'1993-02-08'),
+	(87,3,'est','dolores','Customer Service',400252,'1995-09-01'),
+	(61,3,'vero','assumenda','Board of Directors',188762,'2010-08-25'),
+	(32,2,'illum','ut','Customer Manager',828665,'1985-01-10'),
+	(138,3,'nesciunt','quos','Help Desk',648156,'1979-08-27'),
+	(144,3,'earum','qui','Customer Service',657188,'1983-07-01'),
+	(13,1,'dolores','sit','Customer Manager',296957,'2022-04-23'),
+	(27,2,'expedita','quam','Board of Directors',467684,'1987-09-09'),
+	(116,1,'cum','aut','Customer Support',935748,'1980-03-29'),
+	(102,3,'autem','placeat','Manager',747086,'2010-06-26'),
+	(2,3,'laudantium','iste','Customer Support',904317,'2006-03-03'),
+	(1,2,'inventore','quis','Manager',950738,'2006-02-21'),
+	(41,1,'modi','sint','Customer Service',947192,'1984-04-11'),
+	(47,1,'placeat','aut','Director',360642,'2016-09-30'),
+	(123,1,'consequatur','impedit','Administrator',805100,'1971-11-26'),
+	(65,3,'qui','ut','Help Desk',539625,'2018-10-14'),
+	(145,3,'tenetur','velit','Founder',488586,'2005-03-01'),
+	(60,1,'voluptatibus','eaque','Client Service',268439,'1984-07-17'),
+	(100,3,'nihil','atque','Client Service',762354,'1981-06-26'),
+	(136,3,'labore','qui','Customer Service',524763,'2000-11-30'),
+	(127,3,'quo','iusto','President',804839,'2001-09-27'),
+	(64,3,'voluptatem','dolorum','Customer Support',369025,'2005-09-24')
+;
+
+INSERT INTO Project VALUES 
+	(19425,3,'a','Pune',4781828,1988),
+	(46820,2,'ea','North Oralchester',8720384,2007),
+	(20534,2,'tenetur','Faheytown',2499445,2001),
+	(18716,1,'veritatis','Morarhaven',5600366,1974),
+	(27422,3,'reiciendis','West Amber',6150142,2012),
+	(18617,2,'dolores','New Leannastad',7298848,2004),
+	(41545,1,'quia','Starkmouth',2614732,1985),
+	(13874,3,'est','Maraview',7153335,2002),
+	(31164,1,'rerum','Lake Darylbury',2075820,1982),
+	(18597,1,'ullam','Garrickberg',5537116,1971),
+	(43491,3,'aspernatur','Hermistonstad',1819411,2004),
+	(31940,2,'suscipit','North Robynfurt',4336490,2022),
+	(35719,1,'alias','North Nikitaberg',6851360,1978),
+	(23950,2,'quis','Lake Jammie',1905239,1996),
+	(25632,3,'sint','West Rodolfoport',7924719,2005)
+;
+
